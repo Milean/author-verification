5f32b34aaf73066d0cf3f491798f76370f462e0c
Milean <m.c.vandam@student.tudelft.nl>
2013-04-13 10:51:31 +0200

    Resolved double file after merging.

--- src/authorverification/preprocessing/FilePartitioner.java
+++ src/authorverification/preprocessing/FilePartitioner.java
@@ -1,4 +1,5 @@
 package authorverification.preprocessing;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -13,28 +14,46 @@ public class FilePartitioner {
 	 */
 	public static void main(String[] args) {
 		try {
-			File f = new File("E:\\wikipedia\\html.lst");
+
+			File f = null;
+			int bytesPerFile = 10000000;
+
+			if(args == null || args.length < 2){
+				//f = new File("E:\\wikipedia\\html.lst");
+				System.out.println("Needs two arguments!");
+				System.exit(0);
+			}
+			else{
+				f = new File(args[0]);
+				try{
+					bytesPerFile = Integer.parseInt(args[1]);
+				}
+				catch(Exception ex){
+					ex.printStackTrace();
+				}
+			}
+
 			FileReader fr = new FileReader(f);
 
 			int part = 0;
 			int c = fr.read();
 			while(c != -1){
-			
-				File out = new File("E:\\wikipedia\\html.lst.part"+part);
+
+				File out = new File(f.getAbsolutePath()+".part"+part);
 				FileWriter fw = new FileWriter(out);
 
 				for(int i = 0; c != -1 && i<10000000; i++){
 					fw.write(c);
 					c = fr.read();
 				}
-				
+
 				fw.flush();
 				fw.close();
-				
+
 				part++;
 			}
 			fr.close();
-			
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -42,7 +61,7 @@ public class FilePartitioner {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
